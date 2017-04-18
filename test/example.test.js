c33ababd601b24fa57a72ef1342e1b0706038465
guanyuan <yuanguan89@gmail.com>
2017-04-18 21:51:51 +0800

    test by jest

--- test/example.test.js
+++ test/example.test.js
@@ -1,23 +1,21 @@
-var chai = require('chai');
-var expect = chai.expect;
 var isNullOrEmpty = require('./../index');
 
 describe('fake suite of tests', function() {
   // The tests have an English description...
   it('should return true given ""', function() {
-    expect(isNullOrEmpty("")).to.be(true);
+    expect(isNullOrEmpty("")).toBe(true);
   });
 
   it('should return true given null', function() {
-    expect(isNullOrEmpty(null)).to.be(true);
+    expect(isNullOrEmpty(null)).toBe(true);
   });
 
   it('should return true given undefined', function() {
-    expect(isNullOrEmpty(undefined)).to.be(true);
+    expect(isNullOrEmpty(undefined)).toBe(true);
   });
   
   it('should return false given a string', function() {
-    expect(isNullOrEmpty("Hello World")).to.be(false)
+    expect(isNullOrEmpty("Hello World")).toBe(false)
   });
 
 });
